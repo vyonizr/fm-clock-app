0511851eeb642919262d2d65cf83b4c3f7cad5b5
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-04-29 23:35:48 +0700

    add background center

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -7,7 +7,7 @@ function Background() {
 
   return (
     <div
-      className={`h-screen bg-cover bg-no-repeat transition-all ${
+      className={`h-screen bg-cover bg-center bg-no-repeat transition-all ${
         isDay
           ? "bg-mobile-day md:bg-tablet-day lg:bg-desktop-day"
           : "bg-mobile-night md:bg-tablet-night lg:bg-desktop-night"
